8454fd3dedcb15bf094299fded76b1b423746b75
olafur-andri <olafur1199@gmail.com>
2016-11-13 22:03:40 +0000

    Finished the FAB button!

--- profile-page/script.js
+++ profile-page/script.js
@@ -1,35 +1,12 @@
-window.addEventListener("load", function(e) {
-  var target = null;
-  var languagesFab = document.querySelector(".fab#languages");
-  var languagesIcon = document.querySelector("#languages i");
-  var menuIcon = document.querySelector(".navbar i");
-  var navbar = document.querySelector(".navbar");
-  var navbarContent = document.querySelector(".navbar-content");
-  var navbarContentUl = document.querySelector(".navbar-content ul");
+window.addEventListener("load", function() {
+  var languagesButton = document.getElementById("languages");
+  var footer = document.querySelector(".footer");
+  var footerContent = document.querySelector(".footer-content");
 
-  document.addEventListener("touchstart", function(e) {
-    target = e.target;
-
-    switch (target) {
-      case languagesFab:
-        languagesFabOnStart(e);
-        break;
-      case menuIcon:
-        menuIconOnStart();
-        break;
-    }
-  }, true);
-
-  function languagesFabOnStart(e) {
+  languagesButton.addEventListener("click", function(e) {
     e.preventDefault();
 
-    languagesIcon.classList.add("fade");
-    //languagesFab.classList.add("grow");
-  }
-
-  function menuIconOnStart() {
-    navbar.classList.toggle("active");
-    navbarContent.classList.toggle("grow");
-    navbarContentUl.classList.toggle("visible");
-  }
+    this.classList.toggle("grow");
+    footerContent.classList.toggle("make-visible");
+  }, true);
 }, true);
