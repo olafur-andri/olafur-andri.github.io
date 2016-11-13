4c2ab0523ee3907d17e9ce468967b763786b98aa
olafur-andri <olafur1199@gmail.com>
2016-11-13 22:35:02 +0000

    Minor changes

--- profile-page/script.js
+++ profile-page/script.js
@@ -2,11 +2,23 @@ window.addEventListener("load", function() {
   var languagesButton = document.getElementById("languages");
   var footer = document.querySelector(".footer");
   var footerContent = document.querySelector(".footer-content");
+  var footerIcon = document.querySelector(".footer i");
+  var hamburger = document.querySelector(".navbar-header i");
+  var navbar = document.querySelector(".navbar");
+  var footerIconText = "";
+  var counter = 1;
 
   languagesButton.addEventListener("click", function(e) {
-    e.preventDefault();
+    footerIconText = footerIcon.textContent;
 
+    e.preventDefault();
     this.classList.toggle("grow");
     footerContent.classList.toggle("make-visible");
+  }, true);
+
+  hamburger.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    navbar.classList.toggle("grow");
   }, true);
 }, true);
