669ce49c786646ab53911eb025abc6a31a3e82d0
olafur-andri <olafur1199@gmail.com>
2016-11-22 17:35:20 +0000

    Added IndexedDB
    In order to save user's language preferences.

--- profile-page/script.js
+++ profile-page/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 window.addEventListener("load", function() {
   var main = document.querySelector("main.main");
   var languagesButton = document.getElementById("languages");
@@ -13,9 +15,81 @@ window.addEventListener("load", function() {
   var icelandicContent = document.querySelectorAll(".icelandic");
   var counter = 1;
   var englishText = [];
+  var icelandicText = [];
+  var db;
+  var idbSupported = false;
+  var languageSelection = {
+    icelandic: false
+  }
+  var numRows = 0;
+  var isIcelandic = false;
 
+  // Get all English and Icelandic text
   for (var i = 0; i < englishContent.length; i++) {
     englishText[i] = englishContent[i].textContent;
+    icelandicText[i] = icelandicContent[i].textContent;
+  }
+
+  // IndexedDB for language selections
+  if ("indexedDB" in window) {
+    console.log("indexedDB supported");
+    idbSupported = true;
+  } else {
+    console.log("indexedDB not supported");
+    alert("Your browser does not support indexed databases, meaning you might have difficulties with the language selection");
+    idbSupported = false;
+  }
+
+  if (idbSupported) {
+    var openRequest = indexedDB.open("olafur", 2);
+
+    openRequest.onupgradeneeded = function(e) {
+      console.log("Upgrading and creating object stores...");
+      db = e.target.result;
+
+      if (!db.objectStoreNames.contains("languages")) {
+        db.createObjectStore("languages", { autoIncrement: true });
+      }
+    }
+
+    openRequest.onsuccess = function(e) {
+      console.log("Open request was a success!");
+      db = e.target.result;
+      var transaction = db.transaction(["languages"], "readonly");
+      var store = transaction.objectStore("languages");
+      var request = store.count();
+
+      request.onsuccess = function(e) {
+        numRows = e.target.result;
+        console.log(numRows);
+
+        request = store.get(numRows);
+
+        request.onsuccess = function(e) {
+          var results = e.target.result;
+          isIcelandic = results.icelandic;
+          console.dir(isIcelandic);
+
+          if (isIcelandic) {
+            for (var i = 0; i < englishContent.length; i++) {
+              englishContent[i].textContent = icelandicText[i];
+            }
+
+            english.classList.remove("disabled");
+            icelandic.classList.add("disabled");
+          }
+        };
+
+        request.onerror = function(e) {
+          console.log("Error", e);
+        }
+      }
+    }
+
+    openRequest.onerror = function(e) {
+      console.log("Error");
+      console.dir(e);
+    }
   }
 
   main.addEventListener("click", function() {
@@ -51,6 +125,21 @@ window.addEventListener("load", function() {
         englishContent[i].classList.remove("fade-out");
       }
     }, 400);
+
+    // Update DB
+    var transaction = db.transaction(["languages"], "readwrite");
+    var store = transaction.objectStore("languages");
+    languageSelection.icelandic = true;
+    var request = store.add(languageSelection);
+
+    request.onerror = function(e) {
+      console.log("Error", e.target.error.name);
+      // Some type of error handler
+    }
+
+    request.onsuccess = function(e) {
+      console.log("Icelandic changed to: " + languageSelection.icelandic);
+    }
   }, true);
 
   english.addEventListener("click", function() {
@@ -69,5 +158,14 @@ window.addEventListener("load", function() {
         englishContent[i].classList.remove("fade-out");
       }
     }, 500);
+
+    languageSelection.icelandic = false;
+    var transaction = db.transaction(["languages"], "readwrite");
+    var store = transaction.objectStore("languages");
+    var request = store.add(languageSelection);
+
+    request.onsuccess = function(e) {
+      console.log("Icelandic changed to false");
+    }
   }, true);
 }, true);
