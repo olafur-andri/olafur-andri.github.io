e0f97435d2bc35afbea83988cbc65c3f10bb30f0
olafur-andri <olafur1199@gmail.com>
2016-12-06 09:42:47 +0000

    Added cursor to #logo

--- christmas-carol/script.js
+++ christmas-carol/script.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", function() {
-  
+  var logo = document.getElementById("logo");
+  var cursor = document.getElementById("cursor");
+
+  logo.classList.add("visible");
+  cursor.classList.add("invisible");
+
+  cursor.addEventListener("transitionend", function() {
+    setTimeout(function() {
+      cursor.classList.toggle("invisible");
+    }, 200);
+  }, true);
 }, true);
