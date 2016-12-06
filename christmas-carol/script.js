87dc532673b39820f0dc8282bc32970cbc45b072
olafur-andri <olafur1199@gmail.com>
2016-12-06 10:47:36 +0000

    Added a bit of an animation to the logo

--- christmas-carol/script.js
+++ christmas-carol/script.js
@@ -1,8 +1,17 @@
 window.addEventListener("load", function() {
   var logo = document.getElementById("logo");
   var cursor = document.getElementById("cursor");
+  var container = document.querySelector(".container");
 
-  logo.classList.add("visible");
+  // Fade in for logo
+  setTimeout(function() {
+    logo.classList.add("visible");
+
+    setTimeout(function() {
+      container.removeChild(logo);
+    }, 3000);
+  }, 1000);
+
   cursor.classList.add("invisible");
 
   cursor.addEventListener("transitionend", function() {
@@ -10,4 +19,5 @@ window.addEventListener("load", function() {
       cursor.classList.toggle("invisible");
     }, 200);
   }, true);
+
 }, true);
