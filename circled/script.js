e8e236a6cc623d3ce909ebf0fe48d5f1c9754672
olafur-andri <olafur1199@gmail.com>
2017-08-13 21:33:34 +0000

    Update

--- circled/script.js
+++ circled/script.js
@@ -1,6 +1,48 @@
 class Circled {
   constructor() {
+    // NODES
+    this.main = document.querySelector('main');
+    this.nav = document.querySelector('nav');
+
+    this.onScroll = this.onScroll.bind(this);
+
+    this.addEventListeners();
+    this.populate('home');
+  }
+
+  addEventListeners() {
+    window.addEventListener('scroll', this.onScroll, true);
+  }
+
+  onScroll() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     
+    if (scrollTop >= 5) {
+      this.nav.classList.add('scroll');
+    } else {
+      this.nav.classList.remove('scroll');
+    }
+  }
+
+  populate(address) {
+    let xhr = null;
+
+    if (window.XMLHttpRequest) {
+      xhr = new XMLHttpRequest();
+    } else {
+      xhr = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        return;
+      }
+      
+      this.main.innerHTML = xhr.responseText;
+    };
+
+    xhr.open('GET', 'home.html', true);
+    xhr.send();
   }
 }
 
